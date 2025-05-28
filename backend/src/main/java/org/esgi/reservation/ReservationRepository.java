ae5f8ee3f7f5aae57da6e5352de8bb1c5f1b9a66
CARACATZANIS Quentin <quentinversaille@gmail.com>
2025-05-28 12:26:08 +0200

    admin reservation done

--- backend/src/main/java/org/esgi/reservation/ReservationRepository.java
+++ backend/src/main/java/org/esgi/reservation/ReservationRepository.java
@@ -3,6 +3,11 @@ package org.esgi.reservation;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.UUID;
+
 @ApplicationScoped
 public class ReservationRepository implements PanacheRepository<ReservationEntity> {
+    public ReservationEntity findById(UUID id) {
+        return find("id", id).firstResult();
+    }
 }
---
+++ backend/src/main/java/org/esgi/reservation/resources/in/ReservationUpdateRequest.java
@@ -0,0 +1,8 @@
+package org.esgi.reservation.resources.in;
+
+import java.time.LocalDateTime;
+
+public class ReservationUpdateRequest {
+    public LocalDateTime startDateTime;
+    public LocalDateTime endDateTime;
+}
